0c017fb7e7ca2578b0548949368d23ff535007ba
Pavel-Bashtovoy <68995889+Pavel-Bashtovoy@users.noreply.github.com>
2021-10-24 20:31:40 +0700

    Add Golden ratio optimization method
    Add Golden ratio optimization method

--- Statistic_model_1/Source.cpp
+++ Statistic_model_1/Source.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
 #include <cmath>
 #include <vector>
 #include <algorithm>
 using namespace std;
 const double PI = 3.141592653589793238463;
 const int N = 200;
-const float lambda = 1.0;
-const float tetta = 0.0;
+const float lambda = 2.5;
+//const float tetta = 0.0;
+const float tetta = 1.12002101;
+
+const float delta = 1.0; // 0,1 0,5 1
+const double eps = 1.E-3;
 
 
 //U  равномерно распределенные на отрезке
@@ -90,18 +95,17 @@ float betta_4(float  yArithmeticMean, float D, std::vector<float>& y) {
 }
 
 //оценка максимального правдоподобия;
-float lossMaximumLikelihoodEstimation (float y, float tetta) {
+float lossMaximumLikelihoodEstimation (float y) {
     float lossFunction = 0;
-    
+
     lossFunction = - log(DistributionDensity((y - tetta) / lambda));
   
     return lossFunction;
 }
 //обобщенные радикальные оценки с разными значениями параметра(как минимум три обязательных значения 0.1, 0.5, 1).
 //  функция потерь 
-float lossGeneralizedRadicalAssessments( float y, float tetta ) {
+float lossGeneralizedRadicalAssessments( float y ) {
     float lossFunction = 0;
-    float  delta = 1;
 
     lossFunction = (- 1 / pow( DistributionDensity(0) , delta)) * pow( DistributionDensity((y - tetta) / lambda), delta);
 
@@ -109,9 +113,8 @@ float lossGeneralizedRadicalAssessments( float y, float tetta ) {
 }
 
 //Оценочная функция 
-float evaluationFunction(float y, float tetta) {
+float evaluationFunction(float y) {
     float evaluation = 0;
-    float  delta = 1;
     float c = 1;
 
     evaluation = c * DistributionDensityDerivative( (y - tetta) / lambda ) * pow( DistributionDensity((y - tetta) / lambda), delta - 1 );
@@ -120,9 +123,91 @@ float evaluationFunction(float y, float tetta) {
 }
 /**Использовать выборки, имеющие следующие виды распределений :
     чистое распределение;
-    засоренное распределение с симметричным засорением(равные сдвиги у чистого и засоряющего распределений, масштаб у засоряющего больше в 2 - 3 раза, чем у чистого);
     засоренное распределение с асимметричным засорением
+    засоренное распределение с симметричным засорением(равные сдвиги у чистого и засоряющего распределений, масштаб у засоряющего больше в 2 - 3 раза, чем у чистого);
 */
+// метод золотого сечения (для минимизации функций и нахождения ОМП)
+void golden_ratio_for_omp(double a, double b, double eps, ofstream& out)
+{
+    double f1, f2, x1, x2;
+    double length, PREVlength;
+    double div;
+    length = fabs(b - a);
+    int iter = 0;//номер текущей итерации 
+    x1 = a + 0.381966011 * (b - a);
+    x2 = a + 0.618003399 * (b - a);
+    f1 = lossMaximumLikelihoodEstimation(x1);
+    f2 = lossMaximumLikelihoodEstimation(x2);
+
+    out << "n|\t" << "a\t\t  " << "|\t" << "b\t\t   " << "|" << "b(i)-a(i) \t|" << "x1\t\t  |\t" << "x2\t\t   |\t" << "f1\t\t|\t\t" << "f2" << endl;
+    while (length > eps)
+    {
+        iter++;
+        if (f1 <= f2)
+        {
+            b = x2;
+            x2 = x1;
+            f2 = f1;
+            x1 = a + 0.381966011 * (b - a);
+            f1 = lossMaximumLikelihoodEstimation(x1);
+        }
+        else
+        {
+            a = x1;
+            x1 = x2;
+            f1 = f2;
+            x2 = a + 0.618003399 * (b - a);
+            f2 = lossMaximumLikelihoodEstimation(x2);
+        }
+        PREVlength = length;
+        length = abs(b - a);
+        div = PREVlength / length;
+        out.setf(ios::scientific);
+        out << iter << "|" << a << "|" << b << "|" << length << "|"  << x1 << "|" << x2 << "|" << f1 << "|" << f2 << endl;
+    }
+}
+
+// метод золотого сечения (для минимизации функций и нахождения ОРО)
+void golden_ratio_for_ORO(double a, double b, double eps, ofstream& out)
+{
+    double f1, f2, x1, x2;
+    double length, PREVlength;
+    double div;
+    length = fabs(b - a);
+    int iter = 0;//номер текущей итерации 
+    x1 = a + 0.381966011 * (b - a);
+    x2 = a + 0.618003399 * (b - a);
+    f1 = lossGeneralizedRadicalAssessments(x1);
+    f2 = lossGeneralizedRadicalAssessments(x2);
+
+    out << "n|\t" << "a\t\t  " << "|\t" << "b\t\t   " << "|" << "b(i)-a(i) \t|" << "x1\t\t  |\t" << "x2\t\t   |\t" << "f1\t\t|\t\t" << "f2" << endl;
+    while (length > eps)
+    {
+        iter++;
+        if (f1 < f2)
+        {
+            b = x2;
+            x2 = x1;
+            f2 = f1;
+            x1 = a + 0.381966011 * (b - a);
+            f1 = lossGeneralizedRadicalAssessments(x1);
+        }
+        else
+        {
+            a = x1;
+            x1 = x2;
+            f1 = f2;
+            x2 = a + 0.618003399 * (b - a);
+            f2 = lossGeneralizedRadicalAssessments(x2);
+        }
+        PREVlength = length;
+        length = abs(b - a);
+        div = PREVlength / length;
+        out.setf(ios::scientific);
+        out << iter << "|" << a << "|" << b << "|" << length << "|" << x1 << "|" << x2 << "|" << f1 << "|" << f2 << endl;
+    }
+}
+
 int main()
 {
     float min = 0.0;
@@ -133,6 +218,9 @@ int main()
     vector<float> u2;
     vector<float> x;
     vector<float> y;
+
+    ofstream fout;
+
 
 
     u1.resize(N);
@@ -156,14 +244,22 @@ int main()
         x[i] = GeneratingRandomVariable(u1[i], u2[i]);
     }
 
+    //Найдем случайную величину y  и y`- среднее y , найдем значения функции плотности
+    fout.open("DistributionDensity.txt");
     std::cout << "Найдем y:" << endl;
-    //Найдем случайную величину y  и y`- среднее y 
+    std::cout << 'i' << '|' << "Y[i]    " << '|' << "Func" << endl;
+    fout << "Y[i]    " << '|' << "Func" << endl;
+
     for (int i = 0; i < N; i++)
     {
         y[i] = x[i] * lambda + tetta;
-        std::cout << i << '|' << y[i] << endl;
+        std::cout << i << '|' << y[i] <<'|'<< DistributionDensity(y[i]) << endl;
+
+        fout << y[i] << ','<< DistributionDensity(y[i]) << endl;
+
         sum_y += y[i];
     }
+    fout.close();
 
     /// Среднее арифметическое 
     yArithmeticMean = sum_y / N;
@@ -181,5 +277,15 @@ int main()
      b_4 = betta_4(yArithmeticMean, D, y);
      std::cout << "Коэффициент Эксцесса:" << b_4 << endl;
 
-    return 0;
+     //минимизация для омп
+     fout.open("OMP.txt");
+     golden_ratio_for_omp(min, max, eps, fout);
+     fout.close();
+
+     //минимизация для ORO
+     fout.open("ORO.txt");
+     golden_ratio_for_ORO(min, max, eps, fout);
+     fout.close();
+     
+     return 0;
 }
